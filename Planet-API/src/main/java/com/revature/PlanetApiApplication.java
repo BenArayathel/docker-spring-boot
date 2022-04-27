93e055f925417299b9f8990ea072afb5b614a5e3
BenArayathel <57010399+BenArayathel@users.noreply.github.com>
2022-04-27 00:22:15 -0400

    fixing planet-api

--- Planet-API/src/main/java/com/revature/PlanetApiApplication.java
+++ Planet-API/src/main/java/com/revature/PlanetApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlanetsApiApplication {
+public class PlanetApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlanetsApiApplication.class, args);
+		SpringApplication.run(PlanetApiApplication.class, args);
 	}
 
 }
--- Planet-API/src/test/java/com/revature/PlanetApiApplicationTests.java
+++ Planet-API/src/test/java/com/revature/PlanetApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlanetsApiApplicationTests {
+class PlanetApiApplicationTests {
 
 	@Test
 	void contextLoads() {
